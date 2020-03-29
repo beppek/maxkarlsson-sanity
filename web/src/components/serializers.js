53d4ce3de0b0acb23b9333486348c46b420ef1ff
Max <max@Maxs-MBP.gateway>
2020-03-29 23:02:24 +1100

    ✨Add syntax highlighting

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,15 +1,15 @@
 import React from 'react'
+
+import CodeBlock from './CodeBlock'
+import InlineCode from './InlineCode'
 import Figure from './Figure'
 
 const serializers = {
   types: {
     authorReference: ({ node }) => <span>{node.author.name}</span>,
     mainImage: Figure,
-    code: props => (
-      <pre data-language={props.node.language}>
-        <code>{props.node.code}</code>
-      </pre>
-    )
+    code: props => <CodeBlock language={props.node.language} value={props.node.code} />, 
+    inlineCode: props => <InlineCode language={props.node.language} value={props.node.code} />
   }
 }
 
