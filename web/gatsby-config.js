d7cabd4e09ff80e6443226edee581e9e2ae6b286
Max <max@Maxs-MBP.gateway>
2020-04-18 15:45:16 +1000

    ✨Add typescript support

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,6 +11,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
+    `gatsby-plugin-typescript`,
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
     {
