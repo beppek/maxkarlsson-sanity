56b36c265c9ed612ad18cc8e35035bdef0dfb1f6
Max <max@Maxs-MBP.gateway>
2020-04-18 15:08:55 +1000

    📦Add gatsby source file system

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV || 'development'}`
@@ -11,6 +13,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-source-sanity',
       options: {
