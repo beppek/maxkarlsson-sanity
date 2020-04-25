27d6c9a57324a4c783a497525c7f8fbd1ee71c1b
Max <max@Maxs-MBP.gateway>
2020-04-25 21:02:18 +1000

    ✨Properly set deps, update config and node

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -4,11 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const { format } = require('date-fns')
-const config = require('./config')
+const { format } = require('date-fns');
+const config = require('./config');
 
 async function createBlogPostPages(graphql, actions, reporter) {
-  const { createPage } = actions
+  const { createPage } = actions;
   const result = await graphql(`
     {
       allSanityPost(
@@ -24,7 +24,9 @@ async function createBlogPostPages(graphql, actions, reporter) {
           }
         }
       }
-      allSanityCourse(filter: {slug: {current: {ne: null}}, publishedAt: { ne: null } }) {
+      allSanityCourse(
+        filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+      ) {
         nodes {
           id
           slug {
@@ -33,43 +35,41 @@ async function createBlogPostPages(graphql, actions, reporter) {
         }
       }
     }
-  `)
+  `);
 
-  if (result.errors) throw result.errors
+  if (result.errors) throw result.errors;
 
-  const postEdges = (result.data.allSanityPost || {}).edges || []
-  const courses = (result.data.allSanityCourse || {}).nodes || []
+  const postEdges = (result.data.allSanityPost || {}).edges || [];
+  const courses = (result.data.allSanityCourse || {}).nodes || [];
 
-  courses
-    .forEach((course, index) => {
-      const { id, slug = {} } = course
-      const path = `/${config.routes.learn}/${slug.current}/`
+  courses.forEach(course => {
+    const { id, slug = {} } = course;
+    const path = `/${config.routes.learn}/${slug.current}/`;
 
-      reporter.info(`Creating coure page: ${path}`)
+    reporter.info(`Creating coure page: ${path}`);
 
-      createPage({
-        path,
-        component: require.resolve('./src/templates/CourseTemplate.jsx'),
-        context: { id }
-      })
-    })
+    createPage({
+      path,
+      component: require.resolve('./src/templates/course-template.tsx'),
+      context: { id },
+    });
+  });
 
-  postEdges
-    .forEach((edge, index) => {
-      const { id, slug = {}, publishedAt } = edge.node
-      const dateSegment = format(publishedAt, 'YYYY/MM')
-      const path = `/${config.routes.blog}/${dateSegment}/${slug.current}/`
+  postEdges.forEach(edge => {
+    const { id, slug = {}, publishedAt } = edge.node;
+    const dateSegment = format(publishedAt, 'YYYY/MM');
+    const path = `/${config.routes.blog}/${dateSegment}/${slug.current}/`;
 
-      reporter.info(`Creating blog post page: ${path}`)
+    reporter.info(`Creating blog post page: ${path}`);
 
-      createPage({
-        path,
-        component: require.resolve('./src/templates/blog-post.jsx'),
-        context: { id }
-      })
-    })
+    createPage({
+      path,
+      component: require.resolve('./src/templates/blog-post.tsx'),
+      context: { id },
+    });
+  });
 }
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  await createBlogPostPages(graphql, actions, reporter)
-}
+  await createBlogPostPages(graphql, actions, reporter);
+};
