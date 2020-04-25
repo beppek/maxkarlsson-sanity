5d08eef049634a899d85a074dcb531da04d09103
Max <max@Maxs-MBP.gateway>
2020-04-25 21:00:48 +1000

    ✨Set styles in ssr and browser

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -1,1 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
 import 'typeface-roboto';
+import { GlobalStyles } from './src/styles/global';
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalStyles>{element}</GlobalStyles>
+);
