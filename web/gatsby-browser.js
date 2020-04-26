643a7ab227cddf90064a0477e857b096af55a74f
Max <max@Maxs-MacBook-Pro.local>
2020-04-26 15:55:57 +1000

    💅Global styles in .css file for now

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { GlobalStyles } from './src/styles/global';
+import './src/styles/global.css';
 
 export const wrapRootElement = ({ element }) => (
   <GlobalStyles>{element}</GlobalStyles>
--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -1,8 +1,43 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import fs from 'fs';
 import { GlobalStyles } from './src/styles/global';
+import './src/styles/global.css';
+
+function getFilesFromPath(path, extension) {
+  const dir = fs.readdirSync(path);
+  return dir.filter((elm) => elm.match(new RegExp(`.*.(${extension})`, 'ig')));
+}
 
 export const wrapRootElement = ({ element }) => (
   <GlobalStyles>{element}</GlobalStyles>
 );
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  const files = getFilesFromPath('./public/static', '.woff2');
+  const preload = ['PixelOperator', 'PixelOperator-Bold', 'PixelOperator8'];
+
+  setHeadComponents([
+    files.map((file) => {
+      return preload
+        .map((font) => {
+          const fileBeginning = file.split('-').slice(0, -1).join('-');
+          if (fileBeginning === font) {
+            return (
+              <link
+                key={font}
+                rel="preload"
+                as="font"
+                type="font/woff2"
+                crossOrigin="anonymous"
+                href={`/static/${file}`}
+              />
+            );
+          }
+          return false;
+        })
+        .filter(Boolean);
+    }),
+  ]);
+};
