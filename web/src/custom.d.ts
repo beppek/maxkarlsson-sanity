8f40a51e5ef13e5be21ef194d2380695f00e9b30
Max <max@Maxs-MBP.home>
2020-04-26 13:00:31 +1000

    🏷Add declaration for woff files

--- web/src/custom.d.ts
+++ web/src/custom.d.ts
@@ -2,3 +2,6 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module '*.woff';
+declare module '*.woff2';
